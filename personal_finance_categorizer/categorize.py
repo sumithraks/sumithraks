aba093ab7a2c20f8278d218c9080bdcce5fdfe3d
sumithraks <sumithra.ks@gmail.com>
2025-09-14 14:53:01 +0530

    consolidate multiple statements

--- personal_finance_categorizer/categorize.py
+++ personal_finance_categorizer/categorize.py
@@ -1,6 +1,9 @@
 import csv
+import time
 import os
 from openai import OpenAI
+api_key=<set_api_key>
+client = OpenAI(api_key=api_key)
 
 #This takes a client handle, uploads the PDF and dumps out CSV
 # TODO: If the file is already in CSV, ability to skip it
@@ -88,98 +91,101 @@ Categorize the following 20 transactions into one of these categories:
     return response
 
 #Get the input file
-input_file=input("Enter the complete file path for the file to process:")
+utilization=dict()
+while(True): 
+    input_file=input("Enter the complete file path for the file to process. Enter done if no more files:")
+    if input_file.lower()=='done':
+        break;
 #TODO: Check if file exists before sending to openai
-response=processPDF(client,input_file)
-chatgpt_response = response.output_text
-lines = chatgpt_response.strip().split('\n')
-i=0
-begin_index=None
-end_index=None
+    response=processPDF(client,input_file)
+    chatgpt_response = response.output_text
+    lines = chatgpt_response.strip().split('\n')
+    i=0
+    begin_index=None
+    end_index=None
 
 #The output contains some verbose response. The actual CSV is between ''' and '''
 #Identify the chunk
-for l in (lines):
-    i=i+1
-    if not l.startswith("```") and begin_index is None:
-        begin_index=i
-        continue;
-    if l.startswith("```") and begin_index is not None:
-        end_index=i
-        break
+    for l in (lines):
+        i=i+1
+        if not l.startswith("```") and begin_index is None:
+            begin_index=i
+            continue;
+        if l.startswith("```") and begin_index is not None:
+            end_index=i
+            break
 
-field_separator = "\t";
+    field_separator = "\t";
 # Get the data rows and header rows separately
-header = lines[begin_index].split(field_separator) 
-data_rows = [line.split(field_separator) for line in lines[begin_index+1:end_index]]
-print(header)
-print(data_rows)
-i=0
-utilization=dict()
-chunk=list()
-amounts=list()
-report=list()
+    header = lines[begin_index].split(field_separator) 
+    data_rows = [line.split(field_separator) for line in lines[begin_index+1:end_index]]
+    print(header)
+    print(data_rows)
+    i=0
+    chunk=list()
+    amounts=list()
+    report=list()
 
 #Categorize them using openai by sending chunks of 10
-for r in data_rows:
-    if (len(r) >=3):
-        description = r[1];
-        chunk.append(description)
-        amt = r[2].strip('\"')
-        isdebit=True
-        amt.strip()
-        if amt.endswith("Dr."):
-            amt.replace("Dr.","")
+    for r in data_rows:
+        if (len(r) >=3):
+            description = r[1];
+            chunk.append(description)
+            amt = r[2].strip('\"')
             isdebit=True
-        elif amt.endswith("Cr."):
-            amt.replace("Cr.","")
-            isdebit=False
-        amt = amt.replace("$","")
-        amt = amt.replace(",","")
-        amt=float(amt)
-        if isdebit is not True:
-            amt=-1*amt
-        
-        amounts.append(amt)
-        if (len(chunk)==10):
-            separator = "\n"
-            to_categorize =separator.join(chunk) 
-            response = categorize(client,to_categorize)
-            output_text = response.output_text
-            lines = output_text.strip().split('\n')
-            for i in range(len(chunk)):
-                fields = lines[i].split(" ")
-                line_item = list()
-                line_item=[chunk[i].strip('\"'),fields[1],amounts[i]]
-                report.append(line_item)
-                utilization[fields[1]]=utilization.get(fields[1],0)+amounts[i]
-            chunk.clear()
-            amounts.clear()
+            amt.strip()
+            if amt.endswith("Dr."):
+                amt.replace("Dr.","")
+                isdebit=True
+            elif amt.endswith("Cr."):
+                amt.replace("Cr.","")
+                isdebit=False
+            amt = amt.replace("$","")
+            amt = amt.replace(",","")
+            amt=float(amt)
+            if isdebit is not True:
+                amt=-1*amt
+            
+            amounts.append(amt)
+            if (len(chunk)==10):
+                separator = "\n"
+                to_categorize =separator.join(chunk) 
+                response = categorize(client,to_categorize)
+                output_text = response.output_text
+                lines = output_text.strip().split('\n')
+                for i in range(len(chunk)):
+                    fields = lines[i].split(" ")
+                    line_item = list()
+                    line_item=[chunk[i].strip('\"'),fields[1],amounts[i]]
+                    report.append(line_item)
+                    utilization[fields[1]]=utilization.get(fields[1],0)+amounts[i]
+                chunk.clear()
+                amounts.clear()
 
-if (len(chunk)>0):
-    separator = "\n"
-    to_categorize =separator.join(chunk) 
-    response = categorize(client,to_categorize)
-    output_text = response.output_text
-    lines = output_text.strip().split('\n')
-    for i in range(len(chunk)):
-        fields = lines[i].split(" ")
-        line_item = list()
-        line_item=[chunk[i].strip('\"'),fields[1],amounts[i]]
-        report.append(line_item)
-        utilization[fields[1]]=utilization.get(fields[1],0)+amounts[i]
+    if (len(chunk)>0):
+        separator = "\n"
+        to_categorize =separator.join(chunk) 
+        response = categorize(client,to_categorize)
+        output_text = response.output_text
+        lines = output_text.strip().split('\n')
+        for i in range(len(chunk)):
+            fields = lines[i].split(" ")
+            line_item = list()
+            line_item=[chunk[i].strip('\"'),fields[1],amounts[i]]
+            report.append(line_item)
+            utilization[fields[1]]=utilization.get(fields[1],0)+amounts[i]
 
 
 #Use the input file's base name to form two files
 # Tagging each transaction into categories 
 # Sum of spend in each category
-filename = os.path.basename(input_file)
-categorized_report = "categorized_report_"+filename+".csv"
-with open(categorized_report, 'w', newline='\n') as csvfile:
-    writer = csv.writer(csvfile,delimiter='\t')
-    writer.writerows(report)
+    filename = os.path.basename(input_file)
+    categorized_report = "categorized_report_"+filename+".csv"
+    with open(categorized_report, 'w', newline='\n') as csvfile:
+        writer = csv.writer(csvfile,delimiter='\t')
+        writer.writerows(report)
 
-spend_by_categories = "spend_by_categories_"+filename+".csv"
+spend_by_categories = "spend_by_categories_"+time.time()+".csv"
 file = open(spend_by_categories, 'w')
 for key in utilization:
     file.write(key+","+str(utilization[key])+"\n")
