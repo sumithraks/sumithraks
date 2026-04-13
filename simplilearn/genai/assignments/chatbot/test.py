62277ea92612d8c6d53464ae37f77799640878d8
sumithraks <sumithra.ks@gmail.com>
2026-04-13 17:07:54 +0530

    Create a directory for assignments

---
+++ simplilearn/genai/assignments/chatbot/test.py
@@ -0,0 +1,11 @@
+import gradio as gr
+
+def alternatingly_agree(message, history):
+    if len([h for h in history if h['role'] == "assistant"]) % 2 == 0:
+        return f"Yes, I do think that: {message}"
+    else:
+        return "I don't think so"
+
+gr.ChatInterface(
+    fn=alternatingly_agree, 
+).launch()
